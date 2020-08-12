a05f0b9e159b9ee6a9ca958122c07714705ae675
Bruno <bvrsilva1@gmail.com>
2020-08-12 11:20:47 -0500

    added banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className='app'>
         <Switch>
           <Route path='/checkout'>
+            <Navbar />
             <h1>checkout</h1>
           </Route>
           <Route path='/login'>
@@ -16,6 +18,7 @@ function App() {
           </Route>
           <Route path='/'>
             <Navbar />
+            <Home />
           </Route>
         </Switch>
       </div>
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <img
+        className='homeImage'
+        src='https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZjFjMDMxZmIt/ZjFjMDMxZmIt-OGVmOTM1ZDYt-w3000._CB431004447_.jpg'
+        alt='banner'
+      />
+    </div>
+  )
+}
+
+export default Home
