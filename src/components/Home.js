84742802dcb1d0a25fda0f0202714edd343fe53b
Bruno <bvrsilva1@gmail.com>
2020-08-12 23:49:31 -0500

    implemented product component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import Product from './Product'
 import './Home.css'
+import { products } from '../data/products'
 
 const Home = () => {
   return (
@@ -9,6 +11,45 @@ const Home = () => {
         src='https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZjFjMDMxZmIt/ZjFjMDMxZmIt-OGVmOTM1ZDYt-w3000._CB431004447_.jpg'
         alt='banner'
       />
+
+      <div className='homeRow'>
+        {products.slice(0, 2).map((product) => (
+          <Product
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+            rating={product.rating}
+          />
+        ))}
+      </div>
+
+      <div className='homeRow'>
+        {products.slice(2, 5).map((product) => (
+          <Product
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+            rating={product.rating}
+          />
+        ))}
+      </div>
+
+      <div className='homeRow'>
+        {products.slice(5, 6).map((product) => (
+          <Product
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+            rating={product.rating}
+          />
+        ))}
+      </div>
     </div>
   )
 }
